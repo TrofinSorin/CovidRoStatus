3a3f2a31ac517bbee60023f16f5ef1b6e4be80c3
Trofin Sorin <sorin.trofin@xivic.com>
2020-03-18 14:14:13 +0200

    mapped data on country, added redirect from each country to new route where we get data for that siruta and mapp data, styles

--- src/components/Judet/Judet.jsx
+++ src/components/Judet/Judet.jsx
@@ -2,21 +2,36 @@ import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 //import { Test } from './Judet.styles';
 import axios from 'axios';
+import { useParams} from "react-router";
+import { useHistory } from "react-router-dom";
 
 const Judet = (props) => {
-  const [data, setData] = useState([]);
+  let { id } = useParams();
+  let history = useHistory();
+  const [countyData, setCountyData] = useState({});
 
   useEffect(() => {
     axios.get('https://services7.arcgis.com/I8e17MZtXFDX9vvT/arcgis/rest/services/Coronavirus_romania/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Judete%20asc&resultOffset=0&resultRecordCount=42&cacheHint=true').then(response => {
       const mappedArray = response.data.features.map(item => item.attributes);
+      const county = mappedArray.filter(item => item.SIRUTA_judet == id)[0];
 
-     setData(mappedArray);
+      setCountyData(county);
     })
   }, []);
 
   return (
     <div className="JudetWrapper">
-      Judet
+      <h3 onClick={() => history.push('/')} style={{ cursor: 'pointer'}}>{ '<< Inapoi la harta'}</h3>
+      <h1 style={{ fontSize: '55px'}}>Judet: {countyData.Judete}</h1>
+      <h2 style={{ fontSize: '32px'}}>Regiune dezvoltare: <span style={{ color: 'red'}}>{countyData.Regiune_dezvoltare}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Populatie: <span style={{ color: 'red'}}>{countyData.Populatie}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Cazuri confirmate: <span style={{ color: 'red'}}>{countyData.Cazuri_confirmate}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Persoane in carantina: <span style={{ color: 'red'}}>{countyData.Persoane_in_carantina}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Persoane izolate: <span style={{ color: 'red'}}>{countyData.Persoane_izolate}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Persoane decedate: <span style={{ color: 'red'}}>{countyData.Persoane_decedate}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Persoane vindecate: <span style={{ color: 'red'}}>{countyData.Persoane_vindecate}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Probe in asteptare: <span style={{ color: 'red'}}>{countyData.Probe_in_asteptare}</span></h2>
+      <h2 style={{ fontSize: '32px'}}>Cazuri infirmate: <span style={{ color: 'red'}}>{countyData.Cazuri_infirmate}</span></h2>
     </div>
   );
 }
