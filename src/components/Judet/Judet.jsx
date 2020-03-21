507e01c19048079f54eaf80d28625ad4fe282ff3
Trofin Sorin <sorin.trofin@xivic.com>
2020-03-21 18:56:33 +0200

    fixed loader by moving inside suspense

--- src/components/Judet/Judet.jsx
+++ src/components/Judet/Judet.jsx
@@ -34,55 +34,52 @@ const Judet = props => {
 
   return (
     <div className="JudetWrapper">
-      <div className="county-container">
-        <h3
-          onClick={() => history.push("/")}
-          style={{ cursor: "pointer", fontSize: "2rem" }}
-        >
-          {"<< Inapoi la harta"}
-        </h3>
-        <h1 style={{ fontSize: "55px" }}>Judet: {countyData.Judete}</h1>
-        <h2 style={{ fontSize: "32px" }}>
-          Regiune dezvoltare:{" "}
-          <span style={{ color: "red" }}>{countyData.Regiune_dezvoltare}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Populatie:{" "}
-          <span style={{ color: "red" }}>{countyData.Populatie}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Cazuri confirmate:{" "}
-          <span style={{ color: "red" }}>{countyData.Cazuri_confirmate}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Persoane in carantina:{" "}
-          <span style={{ color: "red" }}>
-            {countyData.Persoane_in_carantina}
-          </span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Persoane izolate:{" "}
-          <span style={{ color: "red" }}>{countyData.Persoane_izolate}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Persoane decedate:{" "}
-          <span style={{ color: "red" }}>{countyData.Persoane_decedate}</span>
-        </h2>
-        {/* <h2 style={{ fontSize: "32px" }}>
-          Persoane vindecate:{" "}
-          <span style={{ color: "red" }}>{countyData.Persoane_vindecate}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Probe in asteptare:{" "}
-          <span style={{ color: "red" }}>{countyData.Probe_in_asteptare}</span>
-        </h2>
-        <h2 style={{ fontSize: "32px" }}>
-          Cazuri infirmate:{" "}
-          <span style={{ color: "red" }}>{countyData.Cazuri_infirmate}</span>
-        </h2> */}
-      </div>
+      <Suspense
+        fallback={
+          <Spin
+            style={{ position: "absolute", top: "50%", left: "50%" }}
+            size="large"
+          />
+        }
+      >
+        <span className="county-container">
+          <h3
+            onClick={() => history.push("/")}
+            style={{ cursor: "pointer", fontSize: "2rem" }}
+          >
+            {"<< Inapoi la harta"}
+          </h3>
+          <h1 style={{ fontSize: "55px" }}>Judet: {countyData.Judete}</h1>
+          <h2 style={{ fontSize: "32px" }}>
+            Regiune dezvoltare:{" "}
+            <span style={{ color: "red" }}>
+              {countyData.Regiune_dezvoltare}
+            </span>
+          </h2>
+          <h2 style={{ fontSize: "32px" }}>
+            Populatie:{" "}
+            <span style={{ color: "red" }}>{countyData.Populatie}</span>
+          </h2>
+          <h2 style={{ fontSize: "32px" }}>
+            Cazuri confirmate:{" "}
+            <span style={{ color: "red" }}>{countyData.Cazuri_confirmate}</span>
+          </h2>
+          <h2 style={{ fontSize: "32px" }}>
+            Persoane in carantina:{" "}
+            <span style={{ color: "red" }}>
+              {countyData.Persoane_in_carantina}
+            </span>
+          </h2>
+          <h2 style={{ fontSize: "32px" }}>
+            Persoane izolate:{" "}
+            <span style={{ color: "red" }}>{countyData.Persoane_izolate}</span>
+          </h2>
+          <h2 style={{ fontSize: "32px" }}>
+            Persoane decedate:{" "}
+            <span style={{ color: "red" }}>{countyData.Persoane_decedate}</span>
+          </h2>
+        </span>
 
-      <Suspense fallback={<Spin size="large" />}>
         <div className="county-map" style={{ pointerEvents: "none" }}>
           <MapComponent />
         </div>
