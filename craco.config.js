d47b7e28731d0b8ad615445481a7ccdd2d96c9c0
Trofin Sorin <sorin.trofin@xivic.com>
2020-03-21 21:43:49 +0200

    mobile install gif and responsive

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,16 @@
-const path = require('path');
-const CracoAlias = require('craco-alias');
+const path = require("path");
+const CracoAlias = require("craco-alias");
 
 module.exports = function({ env, paths }) {
   return {
     webpack: {
       alias: {
-        environment: path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV)
+        environment: path.join(
+          __dirname,
+          "src",
+          "environments",
+          process.env.CLIENT_ENV
+        )
       }
     },
     plugins: [
@@ -13,7 +18,8 @@ module.exports = function({ env, paths }) {
         plugin: CracoAlias,
         options: {
           aliases: {
-            '@shared': 'src/_shared'
+            "@shared": "src/_shared",
+            "@assets": "src/assets"
           }
         }
       }
--- src/components/MobileInstallInfo/MobileInstallInfo.jsx
+++ src/components/MobileInstallInfo/MobileInstallInfo.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { MoreOutlined } from "@ant-design/icons";
+import "./MobileInstallInfo.scss";
+import { useHistory } from "react-router-dom";
 
-const MobileInstallInfo = props => (
-  <div className="MobileInstallInfoWrapper">
-    <h1>Mobile Install Info</h1>
-  </div>
-);
+const MobileInstallInfo = props => {
+  let history = useHistory();
+
+  return (
+    <div className="MobileInstallInfoWrapper">
+      <h3
+        onClick={() => history.push("/")}
+        style={{ cursor: "pointer", fontSize: "2rem" }}
+      >
+        {"<< Inapoi la harta"}
+      </h3>
+
+      <h1>Mobile Install Info</h1>
+      <div
+        className="info-wrapper"
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div
+          className="first-method"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <p>1. Deschideti Google Chrome</p>
+          <p>
+            2. Apasati pe <MoreOutlined />
+          </p>
+          <p>3. Add to Home screen</p>
+        </div>
+
+        <h3 style={{ margin: "3rem" }}>Sau</h3>
+
+        <div className="second-method">
+          <img
+            src={require("../../assets/gifs/mobile-install.gif")}
+            alt="loading..."
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MobileInstallInfo;
