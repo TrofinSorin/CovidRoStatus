117f0365a228bdc90848cce446702f4d2a6ad1bf
Trofin Sorin <sorin.trofin@xivic.com>
2020-03-21 23:37:21 +0200

    harghita styles

--- src/_shared/Judete/Harghita.jsx
+++ src/_shared/Judete/Harghita.jsx
@@ -7,7 +7,7 @@ const Harghita = props => {
         xmlns="http://www.w3.org/2000/svg"
         width="500"
         height="800"
-        viewBox="-50 125 700 600"
+        viewBox="-50 125 800 600"
       >
         <rect style={{ fill: "none" }} width="500" height="800" />
         <a
