51b8a31bfae744784c2941407ec0f42f801b8210
Trofin Sorin <sorin.trofin@xivic.com>
2020-05-19 11:03:39 +0300

    service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -56,7 +56,7 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl, { scope: "/" })
+    .register(swUrl)
     .then((registration) => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
