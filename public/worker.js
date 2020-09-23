d66aa370509431f6ecb7f84023427e10aab99e34
Trofin Sorin <sorin.trofin@xivic.com>
2020-09-23 23:57:26 +0300

    index

--- public/worker.js
+++ public/worker.js
@@ -1,60 +1,3 @@
-// var CACHE_NAME = "pwa-task-manager";
-// var urlsToCache = ["/"];
-
-// // Install a service worker
-// // eslint-disable-next-line no-restricted-globals
-// self.addEventListener("install", (event) => {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(function (cache) {
-//       console.log("Opened cache");
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-// // eslint-disable-next-line no-restricted-globals
-// self.addEventListener("message", (e) => {
-//   console.log("e:", e);
-//   console.log("e.data:", e.data);
-//   if (e.data === "skipWaiting") {
-//     // eslint-disable-next-line no-restricted-globals
-//     self.skipWaiting();
-//   }
-// });
-
-// // Cache and return requests
-// // eslint-disable-next-line no-restricted-globals
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
-
-// // Update a service worker
-// // eslint-disable-next-line no-restricted-globals
-// self.addEventListener("activate", function (event) {
-//   event.waitUntil(
-//     caches.keys().then(function (cacheNames) {
-//       return Promise.all(
-//         cacheNames
-//           .filter(function (cacheName) {
-//             return cacheName.startsWith(CACHE_NAME) && cacheName !== CACHE_NAME;
-//           })
-//           .map(function (cacheName) {
-//             return caches.delete(cacheName);
-//           })
-//       );
-//     })
-//   );
-// });
-
 var CACHE_NAME = "pwa-task-manager";
 var urlsToCache = ["/"];
 
@@ -73,7 +16,6 @@ self.addEventListener("install", (event) => {
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("message", function (e) {
   console.log("e:", e);
-  console.log("e.data:", e.data);
   if (e.data === "skipWaiting") {
     // eslint-disable-next-line no-restricted-globals
     self.skipWaiting();
