aef029528d90bcd27ee33a89e3441ac32dca9cd7
Trofin Sorin <sorin.trofin@xivic.com>
2020-10-01 22:40:11 +0300

    worker

--- public/worker.js
+++ public/worker.js
@@ -1,121 +1,153 @@
+var CACHE_NAME = "pwa-task-manager";
+var urlsToCache = ["/"];
+
+// Install a service worker
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            return cacheName.startsWith(CACHE_NAME) && cacheName !== CACHE_NAME;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
+
+// Detects if device is on iOS
+// const isIos = () => {
+//   const userAgent = window.navigator.userAgent.toLowerCase();
+//   return /iphone|ipad|ipod/.test(userAgent);
+// };
+// // Detects if device is in standalone mode
+// const isInStandaloneMode = () => 'standalone' in window.navigator && window.navigator.standalone;
+
+// // Checks if should display install popup notification:
+// if (isIos() && !isInStandaloneMode()) {
+//   this.setState({ showInstallMessage: true });
+// }
+
+// source: https://googlechrome.github.io/samples/service-worker/basic/
 /*
  Copyright 2016 Google Inc. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
- */
+*/
 
-// Set a name for the current cache
-var cacheName = "v1";
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+// const PRECACHE = 'precache-v1';
+// const RUNTIME = 'runtime';
 
-// Default files to always cache
-var cacheFiles = ["./", "./index.html"];
+// // A list of local resources we always want to be cached.
+// const PRECACHE_URLS = [
+//   'index.html',
+//   './', // Alias for index.html
+//   'styles.css',
+//   '../../styles/main.css',
+//   'demo.js'
+// ];
 
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener("install", function (e) {
-  console.log("[ServiceWorker] Installed");
+// // The install handler takes care of precaching the resources we always need.
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches
+//       .open(PRECACHE)
+//       .then(cache => cache.addAll(PRECACHE_URLS))
+//       .then(
+//         // eslint-disable-next-line no-restricted-globals
+//         self.skipWaiting()
+//       )
+//   );
+// });
 
-  // e.waitUntil Delays the event until the Promise is resolved
-  e.waitUntil(
-    // Open the cache
-    caches.open(cacheName).then(function (cache) {
-      // Add all the default files to the cache
-      console.log("[ServiceWorker] Caching cacheFiles");
-      return cache.addAll(cacheFiles);
-    })
-  ); // end e.waitUntil
-});
+// // The activate handler takes care of cleaning up old caches.
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('activate', event => {
+//   const currentCaches = [PRECACHE, RUNTIME];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then(cacheNames => {
+//         return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+//       })
+//       .then(cachesToDelete => {
+//         return Promise.all(
+//           cachesToDelete.map(cacheToDelete => {
+//             return caches.delete(cacheToDelete);
+//           })
+//         );
+//       })
+//       .then(() =>
+//         // eslint-disable-next-line no-restricted-globals
+//         self.clients.claim()
+//       )
+//   );
+// });
 
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener("activate", function (e) {
-  console.log("[ServiceWorker] Activated");
+// // The fetch handler serves responses for same-origin resources from a cache.
+// // If no response is found, it populates the runtime cache with the response
+// // from the network before returning it to the page.
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('fetch', event => {
+//   // Skip cross-origin requests, like those for Google Analytics.
+//   // eslint-disable-next-line no-restricted-globals
+//   if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then(cachedResponse => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
 
-  e.waitUntil(
-    // Get all the cache keys (cacheName)
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (thisCacheName) {
-          // If a cached item is saved under a previous cacheName
-          if (thisCacheName !== cacheName) {
-            // Delete that cached file
-            console.log(
-              "[ServiceWorker] Removing Cached Files from Cache - ",
-              thisCacheName
-            );
-            return caches.delete(thisCacheName);
-          }
-        })
-      );
-    })
-  ); // end e.waitUntil
-});
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener("fetch", function (e) {
-  console.log("[ServiceWorker] Fetch", e.request.url);
-
-  // e.respondWidth Responds to the fetch event
-  e.respondWith(
-    // Check in cache for the request being made
-    caches
-      .match(e.request)
-
-      .then(function (response) {
-        // If the request is in the cache
-        if (response) {
-          console.log(
-            "[ServiceWorker] Found in Cache",
-            e.request.url,
-            response
-          );
-          // Return the cached version
-          return response;
-        }
-
-        // If the request is NOT in the cache, fetch and cache
-
-        var requestClone = e.request.clone();
-        fetch(requestClone)
-          .then(function (response) {
-            if (!response) {
-              console.log("[ServiceWorker] No response from fetch ");
-              return response;
-            }
-
-            var responseClone = response.clone();
-
-            //  Open the cache
-            caches.open(cacheName).then(function (cache) {
-              // Put the fetched response in the cache
-              cache.put(e.request, responseClone);
-              console.log("[ServiceWorker] New Data Cached", e.request.url);
-
-              // Return the response
-              return response;
-            }); // end caches.open
-          })
-          .catch(function (err) {
-            console.log(
-              "[ServiceWorker] Error Fetching & Caching New Data",
-              err
-            );
-          });
-      }) // end caches.match(e.request)
-  ); // end e.respondWith
-});
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener("message", function (e) {
-  console.log("e:", e);
-  console.log("e.data:", e.data);
-  if (e.data === "skipWaiting") {
-    // eslint-disable-next-line no-restricted-globals
-    self.skipWaiting();
-  }
-});
+//         return caches.open(RUNTIME).then(cache => {
+//           return fetch(event.request).then(response => {
+//             // Put a copy of the response in the runtime cache.
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response;
+//             });
+//           });
+//         });
+//       })
+//     );
+//   }
+// });
