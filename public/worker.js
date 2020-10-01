66610f4ab4da8dd8eee92f0726d40f7f6cbd4189
Trofin Sorin <sorin.trofin@xivic.com>
2020-10-01 22:36:57 +0300

    new fetch

--- public/worker.js
+++ public/worker.js
@@ -11,199 +11,103 @@
  limitations under the License.
  */
 
-var version = "v1::";
+// Set a name for the current cache
+var cacheName = "v1";
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  "./", // Alias for index.html
-];
+// Default files to always cache
+var cacheFiles = ["./", "./index.html"];
 
-// The install handler takes care of precaching the resources we always need.
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener("install", function (event) {
-  console.log("WORKER: install event in progress.");
-  event.waitUntil(
-    /* The caches built-in is a promise-based API that helps you cache responses,
-       as well as finding and deleting them.
-    */
-    caches
-      /* You can open a cache by name, and this method returns a promise. We use
-         a versioned cache name here so that we can remove old cache entries in
-         one fell swoop later, when phasing out an older service worker.
-      */
-      .open(version + "fundamentals")
-      .then(function (cache) {
-        /* After the cache is opened, we can fill it with the offline fundamentals.
-           The method below will add all resources we've indicated to the cache,
-           after making HTTP requests for each of them.
-        */
-        return cache.addAll(PRECACHE_URLS);
-      })
-      .then(function () {
-        console.log("WORKER: install completed");
-      })
-  );
+self.addEventListener("install", function (e) {
+  console.log("[ServiceWorker] Installed");
+
+  // e.waitUntil Delays the event until the Promise is resolved
+  e.waitUntil(
+    // Open the cache
+    caches.open(cacheName).then(function (cache) {
+      // Add all the default files to the cache
+      console.log("[ServiceWorker] Caching cacheFiles");
+      return cache.addAll(cacheFiles);
+    })
+  ); // end e.waitUntil
 });
 
-// The activate handler takes care of cleaning up old caches.
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener("activate", function (event) {
-  /* Just like with the install event, event.waitUntil blocks activate on a promise.
-     Activation will fail unless the promise is fulfilled.
-  */
-  console.log("WORKER: activate event in progress.");
+self.addEventListener("activate", function (e) {
+  console.log("[ServiceWorker] Activated");
 
-  event.waitUntil(
-    caches
-      /* This method returns a promise which will resolve to an array of available
-         cache keys.
-      */
-      .keys()
-      .then(function (keys) {
-        // We return a promise that settles when all outdated caches are deleted.
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              // Filter by keys that don't start with the latest version prefix.
-              return !key.startsWith(version);
-            })
-            .map(function (key) {
-              /* Return a promise that's fulfilled
-                 when each outdated cache is deleted.
-              */
-              return caches.delete(key);
-            })
-        );
-      })
-      .then(function () {
-        console.log("WORKER: activate completed.");
-      })
-  );
+  e.waitUntil(
+    // Get all the cache keys (cacheName)
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (thisCacheName) {
+          // If a cached item is saved under a previous cacheName
+          if (thisCacheName !== cacheName) {
+            // Delete that cached file
+            console.log(
+              "[ServiceWorker] Removing Cached Files from Cache - ",
+              thisCacheName
+            );
+            return caches.delete(thisCacheName);
+          }
+        })
+      );
+    })
+  ); // end e.waitUntil
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener("fetch", function (event) {
-  console.log("WORKER: fetch event in progress.");
+self.addEventListener("fetch", function (e) {
+  console.log("[ServiceWorker] Fetch", e.request.url);
 
-  /* We should only cache GET requests, and deal with the rest of method in the
-     client-side, by handling failed POST,PUT,PATCH,etc. requests.
-  */
-  if (event.request.method !== "GET") {
-    /* If we don't block the event as shown below, then the request will go to
-       the network as usual.
-    */
-    // console.log(
-    //   "WORKER: fetch event ignored.",
-    //   event.request.method,
-    //   event.request.url
-    // );
-    return;
-  }
-
-  /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
-     Fulfillment result will be used as the response, and rejection will end in a
-     HTTP response indicating failure.
-  */
-  event.respondWith(
+  // e.respondWidth Responds to the fetch event
+  e.respondWith(
+    // Check in cache for the request being made
     caches
-      /* This method returns a promise that resolves to a cache entry matching
-         the request. Once the promise is settled, we can then provide a response
-         to the fetch request.
-      */
-      .match(event.request)
-      .then(function (cached) {
-        /* Even if the response is in our cache, we go to the network as well.
-           This pattern is known for producing "eventually fresh" responses,
-           where we return cached responses immediately, and meanwhile pull
-           a network response and store that in the cache.
-           Read more:
-           https://ponyfoo.com/articles/progressive-networking-serviceworker
-        */
-        var networked = fetch(event.request)
-          // We handle the network request with success and failure scenarios.
-          .then(fetchedFromNetwork, unableToResolve)
-          // We should catch errors on the fetchedFromNetwork handler as well.
-          .catch(unableToResolve);
+      .match(e.request)
 
-        /* We return the cached response immediately if there is one, and fall
-           back to waiting on the network as usual.
-        // */
-        // console.log(
-        //   "WORKER: fetch event",
-        //   cached ? "(cached)" : "(network)",
-        //   event.request.url
-        // );
-        return cached || networked;
-
-        function fetchedFromNetwork(response) {
-          /* We copy the response before replying to the network request.
-             This is the response that will be stored on the ServiceWorker cache.
-          */
-          var cacheCopy = response.clone();
-
-          // console.log(
-          //   "WORKER: fetch response from network.",
-          //   event.request.url
-          // );
-
-          caches
-            // We open a cache to store the response for this request.
-            .open(version + "pages")
-            .then(function add(cache) {
-              /* We store the response for this request. It'll later become
-                 available to caches.match(event.request) calls, when looking
-                 for cached responses.
-              */
-
-              cache.put(event.request, cacheCopy);
-            })
-            .then(function () {
-              // console.log(
-              //   "WORKER: fetch response stored in cache.",
-              //   event.request.url
-              // );
-            });
-
-          // Return the response so that the promise is settled in fulfillment.
+      .then(function (response) {
+        // If the request is in the cache
+        if (response) {
+          console.log(
+            "[ServiceWorker] Found in Cache",
+            e.request.url,
+            response
+          );
+          // Return the cached version
           return response;
         }
 
-        /* When this method is called, it means we were unable to produce a response
-           from either the cache or the network. This is our opportunity to produce
-           a meaningful response even when all else fails. It's the last chance, so
-           you probably want to display a "Service Unavailable" view or a generic
-           error response.
-        */
-        function unableToResolve() {
-          /* There's a couple of things we can do here.
-             - Test the Accept header and then return one of the `offlineFundamentals`
-               e.g: `return caches.match('/some/cached/image.png')`
-             - You should also consider the origin. It's easier to decide what
-               "unavailable" means for requests against your origins than for requests
-               against a third party, such as an ad provider
-             - Generate a Response programmaticaly, as shown below, and return that
-          */
+        // If the request is NOT in the cache, fetch and cache
 
-          console.log(
-            "WORKER: fetch request failed in both cache and network."
-          );
+        var requestClone = e.request.clone();
+        fetch(requestClone)
+          .then(function (response) {
+            if (!response) {
+              console.log("[ServiceWorker] No response from fetch ");
+              return response;
+            }
 
-          /* Here we're creating a response programmatically. The first parameter is the
-             response body, and the second one defines the options for the response.
-          */
-          return new Response("<h1>Service Unavailable</h1>", {
-            status: 503,
-            statusText: "Service Unavailable",
-            headers: new Headers({
-              "Content-Type": "text/html",
-            }),
+            var responseClone = response.clone();
+
+            //  Open the cache
+            caches.open(cacheName).then(function (cache) {
+              // Put the fetched response in the cache
+              cache.put(e.request, responseClone);
+              console.log("[ServiceWorker] New Data Cached", e.request.url);
+
+              // Return the response
+              return response;
+            }); // end caches.open
+          })
+          .catch(function (err) {
+            console.log(
+              "[ServiceWorker] Error Fetching & Caching New Data",
+              err
+            );
           });
-        }
-      })
-  );
+      }) // end caches.match(e.request)
+  ); // end e.respondWith
 });
 
 // eslint-disable-next-line no-restricted-globals
