ba9a97d64a89721ddfbbad0c778af5c88b67337c
Trofin Sorin <sorin.trofin@xivic.com>
2020-11-03 01:44:04 +0200

    cache

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -233,7 +233,7 @@ class Home extends Component {
                   </h2>
                   <h2 style={{ fontSize: "28px" }}>
                     Activi:
-                    <span style={{ color: "blue" }}>
+                    <span style={{ color: "red" }}>
                       {arcGisNationalData.active}
                     </span>
                   </h2>
