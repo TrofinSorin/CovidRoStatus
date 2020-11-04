c656ce94e2fbddf132e227ba04a23ae4c5a2a53f
Trofin Sorin <sorin.trofin@xivic.com>
2020-11-04 02:14:09 +0200

    insta

--- src/sw-template.js
+++ src/sw-template.js
@@ -7,14 +7,6 @@ if (typeof importScripts === "function") {
   if (workbox) {
     console.log("Workbox is loaded");
 
-    // eslint-disable-next-line
-    self.addEventListener("message", (event) => {
-      if (event.data && event.data.type === "SKIP_WAITING") {
-        console.log("event.data.type:", event.data.type);
-        workbox.core.skipWaiting();
-      }
-    });
-
     /* injection point for manifest files.  */
     // eslint-disable-next-line
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
@@ -138,4 +130,12 @@ if (typeof importScripts === "function") {
   } else {
     console.log("Workbox could not be loaded. No Offline support");
   }
+
+  // eslint-disable-next-line
+  self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+      console.log("event.data.type:", event.data.type);
+      workbox.core.skipWaiting();
+    }
+  });
 }
