89ec95b27c0e8dccafc96ab7c88084e0ad8777d7
Trofin Sorin <sorin.trofin@xivic.com>
2020-11-09 23:34:09 +0200

    removed

--- src/App.js
+++ src/App.js
@@ -3,25 +3,6 @@ import "./App.scss";
 import Router from "./Router";
 
 function App(props) {
-  useEffect(() => {
-    window["isUpdateAvailable"].then((reg) => {
-      if (reg) {
-        setupListener(reg);
-      }
-    });
-  }, []);
-
-  const setupListener = (reg) => {
-    reg.onupdatefound = () => {
-      const installingWorker = reg.installing;
-      installingWorker.onstatechange = () => {
-        if (installingWorker.state === "installed") {
-          console.log("installingWorker.state:", installingWorker.state);
-        }
-      };
-    };
-  };
-
   return (
     <div className="App">
       <header className="App-header"></header>
